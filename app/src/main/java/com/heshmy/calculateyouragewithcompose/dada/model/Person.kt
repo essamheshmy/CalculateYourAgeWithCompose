ef5e6cd797921c1fef801920cc5b75eee9594da4
essam <essamheshmey>
2024-08-21 08:43:40 +0300

    Create Person class model

---
+++ app/src/main/java/com/heshmy/calculateyouragewithcompose/dada/model/Person.kt
@@ -0,0 +1,11 @@
+package com.heshmy.calculateyouragewithcompose.dada.model
+
+
+//@Entity(tableName = "person_table")
+data class Person(
+ //   @PrimaryKey()
+ //   @ColumnInfo(name = "person_name")
+    val name:String?,
+   // @ColumnInfo(name = "person_age")
+    val age:Int?
+)
