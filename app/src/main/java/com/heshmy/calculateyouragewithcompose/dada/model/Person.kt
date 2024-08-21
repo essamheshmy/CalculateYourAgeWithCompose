f22f2ce57497a8982336c28bcb7a381c94875a1e
essam <essamheshmey>
2024-08-21 09:21:10 +0300

    add kotlin annotation in Person.kt

--- app/src/main/java/com/heshmy/calculateyouragewithcompose/dada/model/Person.kt
+++ app/src/main/java/com/heshmy/calculateyouragewithcompose/dada/model/Person.kt
@@ -1,11 +1,15 @@
 package com.heshmy.calculateyouragewithcompose.dada.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-//@Entity(tableName = "person_table")
+
+@Entity(tableName = "person_table")
 data class Person(
- //   @PrimaryKey()
- //   @ColumnInfo(name = "person_name")
+    @PrimaryKey()
+    @ColumnInfo(name = "person_name")
     val name:String?,
-   // @ColumnInfo(name = "person_age")
+    @ColumnInfo(name = "person_age")
     val age:Int?
 )
