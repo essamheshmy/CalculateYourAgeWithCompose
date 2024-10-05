70da2b182eb7c2352eeaaabfd3ed7f81f399cdf0
essam <essamheshmey>
2024-10-05 18:07:02 +0300

    add ViewModel dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,12 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    val lifecycle_version = "2.8.5"
+    val arch_version = "2.2.0"
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // ViewModel utilities for Compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version")
+
 }
